259150da1936cdd6ad0f94e44df4bc984e48f29f
Christian Koch <ckoch002@student.ucr.edu>
2010-04-21 22:40:40 -0700

    Added some simple, default front pages.

--- carillon.rb
+++ carillon.rb
@@ -219,7 +219,18 @@ end
 ##########
 
 # Front pages
-get ':slug' do
-  @page = DB[:reviews].where(:slug => params[:slug]).first
-  erb :page
+get '/' do
+  @posts = DB[:posts].all
+  @reviews = DB[:reviews].all
+  erb :index
+end
+
+get '/posts/:slug' do
+  @post = DB[:posts].where(:slug => params[:slug]).first
+  erb :post
+end
+
+get '/reviews/:slug' do
+  @review = DB[:reviews].where(:slug => params[:slug]).first
+  erb :review
 end
